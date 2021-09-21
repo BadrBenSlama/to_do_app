5466836fc80e30ff5eda66024f0bb0d67e3ddc01
Goldinium <badrbenslama8@gmail.com>
2021-09-21 11:30:42 +0100

    basic setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './style.css';
+
+const tasks = [
+  {
+    description: 'walk the dog',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'walk the cat',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'walk the bird',
+    completed: true,
+    index: 3,
+  },
+];

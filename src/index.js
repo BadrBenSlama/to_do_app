4657cf51fae899f8050f0d4edd47dd3b05755ac6
Goldinium <badrbenslama8@gmail.com>
2021-09-21 14:03:37 +0100

    setup webpack and index.html and style.css

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 import './style.css';
 
-const tasks = [
+const toDoList = [
   {
-    description: 'walk the dog',
+    description: 'take a walk',
     completed: true,
     index: 1,
   },
   {
-    description: 'walk the cat',
+    description: 'prepare lunch',
     completed: false,
     index: 2,
   },
   {
-    description: 'walk the bird',
+    description: 'take a shower',
     completed: true,
     index: 3,
   },
 ];
+
+const listContainer = document.getElementById('todo_list');
+
+const showtasks = () => {
+  for (let i = 0; i < toDoList.length; i += 1) {
+    const task = toDoList[i];
+    const list = `<li class="task flex" id="${task.index}">
+  <div>
+    <input type="checkbox" class="box" id="list_box">
+    <span>${task.description}</span>
+  </div>
+   <button type="submit"><i class="fas fa-ellipsis-v"></i></button>
+  </li>
+  <div class="border_bottom"></div>`;
+    listContainer.innerHTML += list;
+  }
+};
+
+window.onload = showtasks;
